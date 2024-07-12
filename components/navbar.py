69fd0a770b54adf227801771701f5e9c1fe4b698
Clementine Elwes <clementineelwes@clementines-mbp.home>
2024-07-12 22:22:14 +0100

    components

---
+++ components/navbar.py
@@ -0,0 +1,20 @@
+# Import necessary libraries
+from dash import html
+import dash_bootstrap_components as dbc
+
+# Define the navbar structure
+def Navbar():
+
+    layout = html.Div([
+        dbc.NavbarSimple(
+            children=[
+                dbc.NavItem(dbc.NavLink("Folding Dashboard", href="/page3")),
+            ] ,
+            brand="Folding DashBoard",
+            brand_href="/page1",
+            color="dark",
+            dark=True,
+        ),
+    ])
+
+    return layout
