d5fba54eee3d4db9636a16008394935825b6d6e6
Clementine Elwes <clementineelwes@clementines-mbp.home>
2024-08-06 00:09:01 +0100

    please

--- main.py
+++ main.py
@@ -76,7 +76,8 @@ def update_graph6(gene_selected, pdb_values, residual_selected, mutfrom_selected
     gene_pdbs = page3.gene_pdbs  
     ddg_info = page3.ddg_info    
     pdb_values = page3.get_pdb_values(gene_pdbs, gene_selected)
-    figure = page3.ddg_for_gene_plot(page3.ddg_info, pdb_values, residual_selected, mutfrom_selected, mutto_selected)
+    median_ddg = page3.calculate_median(pdb_values,residual_selected, mutfrom_selected, mutto_selected)
+    figure = page3.ddg_for_gene_plot(page3.ddg_info, pdb_values, median_ddg)
     return figure
 
 ## Callback for ddg by variant
@@ -95,6 +96,29 @@ def update_graph7(gene_selected, pdb_values, residual_selected, mutfrom_selected
     figure = page3.ddg_for_variant_plot(page3.ddg_info, pdb_values, residual_selected, mutfrom_selected, mutto_selected)
     return figure
 
+##Callback for markdown text
+@app.callback(
+    Output(component_id = "gene_ddg_markdown", component_property = "children"),
+    [Input(component_id = "gene_selected", component_property = "value"),
+     Input(component_id = "pdb_values", component_property = "value"),
+     Input(component_id = "residual_selected", component_property = "value"),
+     Input(component_id = "mutfrom_selected", component_property = "value"),
+     Input(component_id = "mutto_selected", component_property = "value"),]
+)
+
+def update_markdown(gene_selected, pdb_values, residual_selected, mutfrom_selected, mutto_selected):
+    gene_pdbs = page3.gene_pdbs
+    ddg_info = page3.ddg_info
+    pdb_values = page3.get_pdb_values(gene_pdbs, gene_selected)
+    median_ddg = page3.calculate_median(pdb_values,residual_selected, mutfrom_selected, mutto_selected)
+    percentile = page3.calculate_percentile(gene_pdbs, gene_selected, ddg_info, residual_selected, mutfrom_selected, mutto_selected)
+    text = page3.gene_ddg_markdown_text(gene_pdbs, gene_selected, ddg_info, residual_selected, mutfrom_selected, mutto_selected, median_ddg)
+    return text
+
+
+
+
+
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
@@ -105,4 +129,4 @@ def display_page(pathname):
 
 # Run the app on localhost:8050
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port='80')
+    app.run_server(debug=True, host='0.0.0.0', port=80)
