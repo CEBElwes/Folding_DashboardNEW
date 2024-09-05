4f53e0c4d24e3c9cfc52478da3cbd15119bdb95a
Clementine Elwes <clementineelwes@clementines-mbp.home>
2024-09-05 16:18:12 +0100

    with full mount

--- pages/page3.py
+++ pages/page3.py
@@ -15,9 +15,9 @@ import seaborn as sns
 
 app = Dash(__name__)
 
-ddg_info = pd.read_csv("ddg_infoNOTCH1.csv")
-gene_pdbs = pd.read_csv("gene_pdbsNOTCH1.csv")
-pdb_residual = pd.read_csv("pdb_residual")
+ddg_info = pd.read_csv("/mount/ddg_info.csv")
+gene_pdbs = pd.read_csv("/mount/gene_pdbs.csv")
+pdb_residual = pd.read_csv("/mount/pdb_residual")
 
 ### ----------------------
 
