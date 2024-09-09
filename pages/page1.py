7a0a6f0de1ce5ae7a7ab3e7b8617938d769d87b6
Clementine Elwes <clementineelwes@eduroam-int-dhcp-97-229-51.ucl.ac.uk>
2024-09-09 17:02:17 +0100

    page1

--- pages/page1.py
+++ pages/page1.py
@@ -16,8 +16,8 @@ import seaborn as sns
 app = Dash(__name__)
 
 ddg_info = pd.read_csv("ddg_infoNOTCH1.csv")
-gene_pdbs = pd.read_csv("gene_pdbsNOTCH1.csv")
-pdb_residual = pd.read_csv("pdb_residual")
+gene_pdbs = pd.read_csv("gene_pdbs")
+pdb_residual = pd.read_csv("/mount/pdb_residual")
 
 ### ----------------------
 
